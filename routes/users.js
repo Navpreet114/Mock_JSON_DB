9b35b2fd0993ba31e2bad8a378f602835c309e8c
Navpreet114 <navpreet.singh@rdpolytech.ca>
2024-11-21 11:43:20 -0700

    first commit

---
+++ routes/users.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+//parse incoming json to native js object
+router.use(express.json());
+
+//takes url encoded strings and decode them for use
+router.use(express.urlencoded({extended: true}));
+
+//define a home route
+router.get('/home', (req, res) => {
+    res.render('home');
+});
+
+//exports the router objects
+module.exports = router;
